524b47e63b620b6acacc341073c046511809ace8
HACO <jasonytonlinecomeandsee@gmail.com>
2023-02-08 23:12:06 +0800

    Upload

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,101 @@
+import { Menu } from "@headlessui/react";
 export default function Navbar() {
-  return <main></main>;
+  return (
+    <div className="navbar bg-base-300">
+      <div className="flex-1">
+        <a className="text-xl normal-case btn btn-ghost">LAZCO</a>
+      </div>
+      <div className="flex-none">
+        <ul className="px-1 menu menu-horizontal">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li tabIndex={0}>
+            <a>
+              Parent
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2 mt-3 bg-base-300">
+              <li>
+                <a>Submenu 1</a>
+              </li>
+              <li>
+                <a>Submenu 2</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </label>
+          <div
+            tabIndex={0}
+            className="mt-3 shadow card card-compact dropdown-content w-52 bg-base-300"
+          >
+            <div className="card-body">
+              <span className="text-lg font-bold">8 Items</span>
+              <span className="text-info">Subtotal: $999</span>
+              <div className="card-actions">
+                <button className="btn btn-primary btn-block">View cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="/images/default-avatar.png" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-300 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#55dba1",
+          secondary: "#f796a8",
+          accent: "#7cf4b2",
+          neutral: "#1F2133",
+          "base-100": "#3F455A",
+          info: "#4DD0F5",
+          success: "#1C7D5D",
+          warning: "#F1CD5F",
+          error: "#EE777F",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
