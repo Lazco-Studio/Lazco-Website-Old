1ae59aa0fd7845e3ca6ad7ebae7861218076a122
HACO <jasonytonlinecomeandsee@gmail.com>
2023-02-09 23:11:10 +0800

    Upload

--- components/home/top.js
+++ components/home/top.js
@@ -1,7 +1,7 @@
 export default function Top() {
   return (
     <div className="relative top h-[640px]">
-      <div className="bg-cover bg-center h-[640px] bg-[url('https://images.unsplash.com/photo-1451187580459-43490279c0fa')]"></div>
+      <div className="bg-cover bg-center h-[640px] bg-[url('/images/home-bg.png')]"></div>
       <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
       <div className="absolute inset-0 p-2 content h-full max-w-[640px] m-auto flex flex-col justify-center items-start text-center	 max-[600px]:items-center	">
         <h1 className="text-white text-4xl font-bold max-[600px]:text-3xl">
