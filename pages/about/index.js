dd43d2fc9328fac4d17a317eaec2cd60357949e8
HACO <jasonytonlinecomeandsee@gmail.com>
2023-02-09 23:16:43 +0800

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -12,7 +12,7 @@ export default function About() {
     <main>
       <div className="flex flex-col items-center justify-center p-20 pt-10 max-[600px]:p-2">
         <h1 className="mb-6 text-4xl font-bold text-base-500">關於我們</h1>
-        <div className="grid grid-cols-2 gap-20 mt-5 max-[1000px]:flex max-[1000px]:flex-wrap">
+        <div className="grid grid-cols-2 gap-20 mt-5 max-[1200px]:flex max-[1200px]:flex-wrap">
           <div className="relative flex flex-col items-center p-8 transition duration-500 ease-in-out transform rounded-lg shadow-md bg-base-300 hover:scale-105">
             <Image
               src="/images/haco.png"
@@ -29,24 +29,24 @@ export default function About() {
               等多種程式語言。憑藉著對網頁開發的熱情和技能，我有信心能夠創造出具有創新性和動感的網站和應用程式，為所有人帶來出色的體驗，
               我很喜歡交朋友也歡迎大寄來我的社群找我喔！對了我也很喜歡出去玩或是參加各種活動，運氣好的話說不定還能遇到我喔！
             </p>
-            <div className="absolute bottom-0 flex flex-wrap mb-8">
+            <div className="absolute bottom-0 flex flex-wrap items-center mb-8 text-center">
               <p className="hover:cursor-pointer">
-                <FaGlobe className="mr-4 text-3xl text-base-500" />
+                <FaGlobe className="ml-4 mr-4 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
                 <FaEnvelope className="mr-4 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
-                <FaGithub className="mr-4 text-3xl text-base-500" />
+                <FaGithub className="mr-2 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
-                <FaInstagram className="mr-4 text-3xl text-base-500" />
+                <FaInstagram className="ml-2 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
-                <FaYoutube className="mr-4 text-3xl text-base-500" />
+                <FaYoutube className="ml-4 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
-                <FaDiscord className="mr-4 text-3xl text-base-500" />
+                <FaDiscord className="ml-4 mr-4 text-3xl text-base-500" />
               </p>
             </div>
           </div>
@@ -65,24 +65,24 @@ export default function About() {
               我的專長包括Vue、React、Golang、C
               和SQL/NoSQL。我是一個自我激勵的人，總是尋找新的挑戰和機會，作為開發者成長。我對我的能力提供高質量的結果有信心，並且始終渴望學習和擴展我的技能集。
             </p>
-            <div className="absolute bottom-0 flex flex-wrap items-center mb-8">
+            <div className="absolute bottom-0 flex flex-wrap items-center mb-8 text-center">
               <p className="hover:cursor-pointer">
-                <FaGlobe className="mr-4 text-3xl text-base-500" />
+                <FaGlobe className="ml-4 mr-4 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
                 <FaEnvelope className="mr-4 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
-                <FaGithub className="mr-4 text-3xl text-base-500" />
+                <FaGithub className="mr-2 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
-                <FaInstagram className="mr-4 text-3xl text-base-500" />
+                <FaInstagram className="ml-2 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
-                <FaYoutube className="mr-4 text-3xl text-base-500" />
+                <FaYoutube className="ml-4 text-3xl text-base-500" />
               </p>
               <p className="hover:cursor-pointer">
-                <FaDiscord className="mr-4 text-3xl text-base-500" />
+                <FaDiscord className="ml-4 mr-4 text-3xl text-base-500" />
               </p>
             </div>
           </div>
