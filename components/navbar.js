b3647bb10a1d823fdc11a1e875fed1c24f87e2da
banahaker <pn0818x@outlook.com>
2023-02-10 15:16:36 +0800

    feat: rwd of navbar

--- components/navbar.js
+++ components/navbar.js
@@ -13,9 +13,9 @@ export default function Navbar() {
           LAZCO
         </a>
       </div>
-      <div className="flex-none">
+      <div className="flex-none max-[640px]:hidden">
         <ul className="px-1 menu menu-horizontal">
-          <li tabIndex={0}>
+          <li tabIndex={0} className="dropdown">
             <p>
               服務項目
               <svg
@@ -28,9 +28,46 @@ export default function Navbar() {
                 <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
               </svg>
             </p>
-            <ul className="p-2 mt-3 bg-base-300">
+            <ul className="p-2 mt-3 bg-base-300 dropdown-content">
               <li>
                 <p>網站製作</p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p onClick={() => router.push("/about")}>關於我們</p>
+          </li>
+          <li>
+            <p onClick={() => router.push("/contact")}>聯絡我們</p>
+          </li>
+        </ul>
+      </div>
+      <div className="dropdown dropdown-end min-[640px]:hidden">
+        <label tabIndex={0} className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h7"
+            />
+          </svg>
+        </label>
+        <ul
+          tabIndex={0}
+          className="menu  dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li tabIndex={0}>
+            <span>服務項目</span>
+            <ul className="rounded-box p-2 bg-base-100">
+              <li>
+                <a>網頁開發</a>
               </li>
             </ul>
           </li>
