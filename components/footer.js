339116619c0f2af9c6ba3247e79cb4979975ac3d
HACO <jasonytonlinecomeandsee@gmail.com>
2023-02-10 22:43:23 +0800

    Updateeeeeeeeeeeeeeeeeee

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="footer h-[80px]">
+      <span className="m-auto text-xl font-thin">
+        Copyright © 2022 Lazco Team All rights reserved.
+      </span>
+    </div>
+  );
+}
