60facb38956b9a5f2a35ee85edabac6ab559bfc9
HACO <jasonytonlinecomeandsee@gmail.com>
2023-02-10 22:43:32 +0800

    Delete footer.jsx

--- components/footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="footer h-[150px]">
-      <span className="text-xl font-thin m-auto">
-        Copyright © 2022 Lazco All rights reserved.
-      </span>
-    </div>
-  );
-}
