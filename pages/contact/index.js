38487f9fd6d163db21cd36266883587639ad803d
HACO <jasonytonlinecomeandsee@gmail.com>
2023-02-10 22:08:20 +0800

    Update

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,10 +1,55 @@
-import Image from "next/image";
+import { useRouter } from "next/router";
+import Swal from "sweetalert2";
+
 export default function Contact() {
+  const router = useRouter();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const endpoint = "/api/contact";
+    const urlencoded = new URLSearchParams();
+    if (!event.target.name.value) return alert("你必須輸入姓名！");
+    if (!event.target.mail.value) return alert("你必須輸入姓名！");
+    if (!event.target.phone.value) return alert("你必須輸入姓名！");
+    if (!event.target.message.value) return alert("你必須輸入姓名！");
+    urlencoded.append("name", event.target.name.value);
+    urlencoded.append("mail", event.target.mail.value);
+    urlencoded.append("phone", event.target.phone.value);
+    urlencoded.append("message", event.target.message.value);
+    if (event.target.discord) {
+      urlencoded.append("discord", event.target.discord.value);
+    }
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: urlencoded,
+    };
+    const response = await fetch(endpoint, options);
+    await response.json();
+    Swal.fire({
+      title: "成功送出聯絡資訊！",
+      icon: "success",
+      confirmButtonText: "確認",
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+      confirmButtonColor: "#55dba1",
+      customClass: {
+        container: "select-none",
+      },
+      focusConfirm: false,
+    }).then((resault) => {
+      router.push("/");
+    });
+  };
   return (
     <main className="my-10">
-      <h1 className="mb-6 text-4xl font-medium text-center">聯絡我們</h1>
+      <h1 className="mb-[44px] text-4xl font-medium text-center">聯絡我們</h1>
       <div className="flex items-center justify-center">
-        <form className="w-3/4 p-10 rounded-lg shadow-xl bg-base-300">
+        <form
+          className="w-3/4 p-10 rounded-lg shadow-xl bg-base-300"
+          onSubmit={handleSubmit}
+        >
           <div className="mb-5">
             <label htmlFor="name" className="flex text-lg text-base-500">
               姓名<p className="text-red-500">*</p>
