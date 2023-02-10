1b4c747a122091ad5e88f276cce6095ee61170ec
banahaker <pn0818x@outlook.com>
2023-02-10 22:41:27 +0800

    feat: footer but idk what to put

---
+++ components/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="footer h-[150px]">
+      <span className="text-xl font-thin m-auto">
+        Copyright © 2022 Lazco Team All rights reserved.
+      </span>
+    </div>
+  );
+}
