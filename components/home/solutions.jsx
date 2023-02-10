50d5087598c1fe507f6c653649be00737fcb8f4a
HACO <jasonytonlinecomeandsee@gmail.com>
2023-02-10 22:09:43 +0800

    Update solutions.jsx

--- components/home/solutions.jsx
+++ components/home/solutions.jsx
@@ -1,18 +1,20 @@
+import { useRouter } from "next/router";
 export default function Solutions() {
+  const router = useRouter();
   return (
-    <div className="solutions py-20 px-3 flex flex-col items-center w-full bg-neutral">
+    <div className="flex flex-col items-center w-full px-3 py-20 solutions bg-neutral">
       <h1 className="text-4xl text-center font-bold max-[640px]:text-3xl">
         解決方案
       </h1>
-      <div className="list mt-10 flex flex-wrap p-3 gap-10 items-center justify-center">
+      <div className="flex flex-wrap items-center justify-center gap-10 p-3 mt-10 list">
         <div className="card w-[375px] bg-base-100 shadow-xl image-full max-[600px]:w-[80vw]">
           <figure>
             <img src="/images/develop.jpg" alt="develop" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title text-2xl text-white">網頁開發</h2>
+            <h2 className="text-2xl text-white card-title">網頁開發</h2>
             <p>提供自定性高的網頁設計/開發服務</p>
-            <div className="card-actions justify-end">
+            <div className="justify-end card-actions">
               <button
                 className="btn btn-primary"
                 onClick={() => router.push("/contact")}
@@ -27,9 +29,9 @@ export default function Solutions() {
             <img src="/images/adviser.jpg" alt="adviser" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title text-2xl text-white">數位顧問</h2>
+            <h2 className="text-2xl text-white card-title">數位顧問</h2>
             <p>提供系統開發顧問，並協助規劃與提供數位轉型建議</p>
-            <div className="card-actions justify-end">
+            <div className="justify-end card-actions">
               <button
                 className="btn btn-primary"
                 onClick={() => router.push("/contact")}
@@ -44,9 +46,9 @@ export default function Solutions() {
             <img src="/images/support.jpg" alt="support" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title text-2xl text-white">客戶服務</h2>
+            <h2 className="text-2xl text-white card-title">客戶服務</h2>
             <p>提供產品售後的問題引導以及解決</p>
-            <div className="card-actions justify-end">
+            <div className="justify-end card-actions">
               <button
                 className="btn btn-primary"
                 onClick={() => router.push("/contact")}
