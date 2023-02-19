a4f30b17fefc06b54c89569ec3e99483ec7fe42c
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-02-20 03:54:02 +0800

    Improve file structure of home page
    -Remove useless router

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import Layout from "@/components/layout";
 import { ReCaptchaProvider } from "next-recaptcha-v3";
 
+
 export default function App({ Component, pageProps }) {
   return (
     <ReCaptchaProvider reCaptchaKey="6LdbjZYkAAAAABy-baRdHf36BxSx1Q2Dz_alLbcw">
