988dddad0df68749556778095e7119975e0c8223
HACO <jasonytonlinecomeandsee@gmail.com>
2023-02-19 22:54:17 +0800

    Update Recaptcha

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout";
+import { ReCaptchaProvider } from "next-recaptcha-v3";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ReCaptchaProvider reCaptchaKey="6LdbjZYkAAAAABy-baRdHf36BxSx1Q2Dz_alLbcw">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ReCaptchaProvider>
   );
 }
