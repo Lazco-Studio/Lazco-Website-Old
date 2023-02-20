b49d03e9816d639ac16cf30b0da7f69743a00151
Alex <anonymousaaaa41414141@gmail.com>
2023-02-20 16:34:10 +0800

    Improve error handeling
    -Add function to check webhook's response
    -Correct response code

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function Home(req, res) {
-  res.status(200).json({ Hello: "World" });
+    res.status(200).json({ Hello: "World" });
 }
