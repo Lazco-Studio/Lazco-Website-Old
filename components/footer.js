86c357fbcad6eedaaf5dd20617d8e8fdfef3e005
Haco <haco@lazco.dev>
2025-01-09 19:57:52 +0800

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer footer-center h-[90px] bg-base-300 text-base-content text-lg row-start-3 row-end-4">
       <div>
-        <p>Copyright © 2023 - All right reserved by Lazco</p>
+        <p>Copyright © 2024 - All right reserved by Lazco Studio.</p>
       </div>
     </footer>
   );
